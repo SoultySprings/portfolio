6c6e80044833f2bdd6f9caeedd523a9085117278
Tanishq <132868290+SoultySprings@users.noreply.github.com>
2025-06-30 16:39:58 +0530

    300625

--- src/app/info/info.ts
+++ src/app/info/info.ts
@@ -10,13 +10,29 @@ export class Info {
   dispProjs = false
   dispContact = false
   dispAbout = false
+  dispResume = false
+  willdisRes(){
+    this.dispProjs = false
+    this.dispAbout = false
+    this.dispContact = false
+    this.dispResume = !this.dispResume
+  }
   willdisProjs(){
+    this.dispResume = false
+    this.dispContact = false
     this.dispProjs = !this.dispProjs
+    this.dispAbout = false
   }
   willdisCont(){
+    this.dispResume = false
+    this.dispProjs = false
     this.dispContact = !this.dispContact
+    this.dispAbout = false
   }
   willdisAbout(){
+    this.dispResume = false
+    this.dispContact = false
     this.dispAbout = !this.dispAbout
+    this.dispProjs = false
   }
 }
