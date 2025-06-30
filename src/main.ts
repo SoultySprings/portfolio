19bcff10ea7cde4fa86bb7b425b961d2b29f4b46
Tanishq <132868290+SoultySprings@users.noreply.github.com>
2025-06-30 19:44:30 +0530

    300625

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+// import { AppComponent } from './app.component';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
